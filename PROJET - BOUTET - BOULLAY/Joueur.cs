7ee95c810437cd4a8d3239b59b3c2cf09aa8c35d
Tinmoiroine <charles.boullay04@gmail.com>
2024-11-11 18:48:29 +0100

    Mise à jour 11/11/2024 - 18h45 - Charles

---
+++ PROJET - BOUTET - BOULLAY/Joueur.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJET___BOUTET___BOULLAY
+{
+    internal class Joueur
+    {
+    }
+}
