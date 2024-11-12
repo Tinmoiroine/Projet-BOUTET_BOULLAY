7cf9a1a1c8760f9bae6004bfa0244b44d00dfbdc
Tinmoiroine <charles.boullay04@gmail.com>
2024-11-12 09:09:30 +0100

    Mise à jour 12/11/2024 - 9h09 - Charles

---
+++ PROJET - BOUTET - BOULLAY/De.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJET___BOUTET___BOULLAY
+{
+    internal class De
+    {
+
+    }
+}
