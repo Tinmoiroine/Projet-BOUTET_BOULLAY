241939f25fc6a3d1a20b73912220acd4072131c8
Tinmoiroine <charles.boullay04@gmail.com>
2024-11-12 14:26:07 +0100

    Mise à jour Classe Joueur

--- PROJET - BOUTET - BOULLAY/Joueur.cs
+++ PROJET - BOUTET - BOULLAY/Joueur.cs
@@ -8,5 +8,16 @@ namespace PROJET___BOUTET___BOULLAY
 {
     internal class Joueur
     {
+        private string nom; 
+        private List<string> mots;
+        private int score;
+
+        public Joueur(string nom)
+        {
+            this.nom = nom;
+            this.score = 0;
+            this.mots = new List<string>();
+        }
+
     }
 }
